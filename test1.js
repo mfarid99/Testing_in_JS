89702ef8f5c2c163fc77b6abc0f1535d44ef0f69
Moe <moe@triblio.com>
2021-12-28 23:32:30 -0500

    basic js test

---
+++ test1.js
@@ -0,0 +1,19 @@
+const sum = (a, b) => {
+    return a - b
+}
+
+// console.log(sum(5, 4))
+
+const subtract = (a, b) => {
+    return a - b
+}
+
+// console.log('subtract = ', subtract(90, 10))
+
+let result = sum(90, 10)
+// console.log('result = ', result)
+let expected = 100
+
+if(result !== expected){
+    throw new Error(`${result} is not equal to ${expected}`)
+}
